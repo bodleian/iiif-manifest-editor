a1876184643361490ffcee334ca8b8ff351195e4
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-06-22 14:53:32 +0200

    Fetch remote manifest JSON and update state of component

--- app/components/OpenManifest.jsx
+++ app/components/OpenManifest.jsx
@@ -1,45 +1,67 @@
 var React = require('react');
 var {Link} = require('react-router');
 
-var OpenManifest = (props) => {
-  return(
-    <div className="open-manifest-container">
-      <div className="open-manifest-form-container">
-        <div className="open-manifest-form-header">
-          <h3>Open Manifest</h3>
-        </div>
-        <div className="drop-manifest-container">
-          <p>Drag and drop manifest here</p>
-        </div>
-        <form className="form-horizontal" role="form">
-          <div className="form-group">
-            <label htmlFor="localManifestFile" className="col-sm-2 control-label">From Computer</label>
-            <div className="col-sm-8">
-              <input type="file" className="form-control" id="localManifestFile" placeholder="Select manifest to open" />
-            </div>
-            <div className="col-sm-2">
-              <button type="submit" className="btn btn-default">Open Manifest</button>
-            </div>
-          </div>
+var OpenManifest = React.createClass({
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+  onLocalManifestFileSubmit: function(e) {
+    e.preventDefault();
+  },
+  onRemoteManifestUrlSubmit: function(e) {
+    e.preventDefault();
+    var remoteManifestUrl = this.refs.remoteManifestUrl.value;
+    if(remoteManifestUrl.length > 0) {
+      this.refs.remoteManifestUrl.value = '';
+      console.log(remoteManifestUrl);
+      this.serverRequest = $.get(remoteManifestUrl, function(result){
+        console.log(result);
+      });
 
-          <div className="form-group">
-            <label htmlFor="remoteManifestUrl" className="col-sm-2 control-label">From URL</label>
-            <div className="col-sm-8">
-              <input type="text" className="form-control" id="remoteManifestUrl" placeholder="Enter URL for manifest to load" />
-            </div>
-            <div className="col-sm-2">
-              <button type="submit" className="btn btn-default">Load Manifest</button>
-            </div>
+    }
+  },
+  render: function() {
+    return(
+      <div className="open-manifest-container">
+        <div className="open-manifest-form-container">
+          <div className="open-manifest-form-header">
+            <h3>Open Manifest</h3>
           </div>
-        </form>
-        <div className="row cancel-button-container">
-          <div className="col-md-12">
-            <Link to="/" className="btn btn-default">Cancel</Link>
+          <div className="drop-manifest-container">
+            <p>Drag and drop manifest here</p>
+          </div>
+          <form className="form-horizontal" role="form" onSubmit={this.onLocalManifestFileSubmit}>
+            <div className="form-group">
+              <label htmlFor="localManifestFile" className="col-sm-2 control-label">From Computer</label>
+              <div className="col-sm-8">
+                <input type="file" className="form-control" id="localManifestFile" placeholder="Select manifest to open" ref="localManifestFile" />
+              </div>
+              <div className="col-sm-2">
+                <button type="submit" className="btn btn-default">Open Manifest</button>
+              </div>
+            </div>
+          </form>
+
+          <form className="form-horizontal" role="form" onSubmit={this.onRemoteManifestUrlSubmit}>
+            <div className="form-group">
+              <label htmlFor="remoteManifestUrl" className="col-sm-2 control-label">From URL</label>
+              <div className="col-sm-8">
+                <input type="text" className="form-control" id="remoteManifestUrl" placeholder="Enter URL for manifest to load" ref="remoteManifestUrl" />
+              </div>
+              <div className="col-sm-2">
+                <button type="submit" className="btn btn-default">Load Manifest</button>
+              </div>
+            </div>
+          </form>
+          <div className="row cancel-button-container">
+            <div className="col-md-12">
+              <Link to="/" className="btn btn-default">Cancel</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+});
 
 module.exports = OpenManifest;
