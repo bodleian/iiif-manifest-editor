d1f8f9baabe745ed3a863fd646f0fdb6ebcee7b0
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-07-01 17:28:12 +0200

    Render edit components conditionally only when mainfest data exists

--- app/components/EditManifest.jsx
+++ app/components/EditManifest.jsx
@@ -7,22 +7,27 @@ var ThumbnailStrip = require('ThumbnailStrip');
 var EditManifest = React.createClass({
   componentWillMount: function() {
     var {manifestData} = this.props;
-    if(manifestData.data === undefined) {
+    if(manifestData === undefined || manifestData.data === undefined) {
       window.location.hash = '#/';
     }
   },
   render: function() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-8">
-            <Viewer/>
-            <ThumbnailStrip/>
+    var {manifestData} = this.props;
+    if(manifestData === undefined || manifestData.data === undefined) {
+      return false;
+    } else {
+      return (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-8">
+              <Viewer/>
+              <ThumbnailStrip/>
+            </div>
+            <MetadataSidebar/>
           </div>
-          <MetadataSidebar/>
         </div>
-      </div>
-    );
+      );
+    }
   }
 });
 
