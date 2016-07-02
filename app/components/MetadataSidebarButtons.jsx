de8eb30da9dcf9427991db4dff0ab298415bf3de
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-07-02 19:40:36 +0200

    Implemented downloading generated/updated manifest data as JSON

--- app/components/MetadataSidebarButtons.jsx
+++ app/components/MetadataSidebarButtons.jsx
@@ -1,17 +1,33 @@
 var React = require('react');
 var {Link} = require('react-router');
+var {connect} = require('react-redux');
 
 var MetadataSidebarButtons = React.createClass({
+  downloadManifestData: function() {
+  var {manifestData} = this.props;
+  var manifestDataJson = JSON.stringify(manifestData.data, null, '\t');
+  var a = document.createElement('a');
+  var blob = new Blob([manifestDataJson], {'type':'application/json'});
+  a.href = window.URL.createObjectURL(blob);
+  a.download = 'manifest.json';
+  a.click();
+},
   render: function() {
     return (
       <div className="metadata-sidebar-buttons">
         <Link to="/new" className="btn btn-default metadata-sidebar-button">New</Link>
         <Link to="/open" className="btn btn-default metadata-sidebar-button">Open</Link>
-        <Link to="/save" className="btn btn-default metadata-sidebar-button">Save</Link>
+        <a onClick={this.downloadManifestData} className="btn btn-default metadata-sidebar-button">Save</a>
         <Link to="/" className="btn btn-default metadata-sidebar-button">Close</Link>
       </div>
     );
   }
 });
 
-module.exports = MetadataSidebarButtons;
+module.exports = connect(
+  (state) => {
+    return {
+      manifestData: state.manifestReducer.manifestData
+    };
+  }
+)(MetadataSidebarButtons);
