7c62183eafc77730575a96f34c936e96cd1e0426
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-07-04 12:12:26 +0200

    Wired up viewer component to update on state changes from the redux store

--- app/components/Viewer.jsx
+++ app/components/Viewer.jsx
@@ -3,9 +3,18 @@ var {connect} = require('react-redux');
 var actions = require('actions');
 
 var Viewer = React.createClass({
+  shouldComponentUpdate(newProps) {
+    return newProps.manifestData !== this.props.manifestData;
+  },
   render: function() {
+    var {manifestData} = this.props;
     return (
       <div className="row viewer-container">
+        <div className="col-md-6">
+          <p><strong>Label</strong>: {manifestData.label}</p>
+          <p><strong>Attribution</strong>: {manifestData.attribution}</p>
+          <p><strong>License</strong>: {manifestData.license}</p>
+        </div>
       </div>
     );
   }
