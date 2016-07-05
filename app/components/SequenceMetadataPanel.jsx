aa5674d766a082084f05138bfb1efeb2769ca887
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-07-05 22:45:05 +0200

    Connected sequence metadata panel to redux store and integrated manifesto

--- app/components/SequenceMetadataPanel.jsx
+++ app/components/SequenceMetadataPanel.jsx
@@ -1,15 +1,29 @@
 var React = require('react');
+var {connect} = require('react-redux');
+var manifesto = require('manifesto.js');
+var EditableTextArea = require('EditableTextArea');
 
 var SequenceMetadataPanel = React.createClass({
-  extractSequenceMetadata: function() {
-
-  },
   render: function() {
+    var {manifestData} = this.props;
+    var manifestoObject = manifesto.create(JSON.stringify(manifestData));
+    var sequence = manifestoObject.getSequenceByIndex(0);
+
     return (
       <div className="metadata-sidebar-panel">
+        <div className="row">
+          <div className="col-md-3 metadata-field-label">Label:</div>
+          <EditableTextArea classNames="col-md-9 metadata-field-value" fieldName="label" value={sequence.getLabel()} onUpdateHandler={()=>{}}/>
+        </div>
       </div>
     );
   }
 });
 
-module.exports = SequenceMetadataPanel;
+module.exports = connect(
+  (state) => {
+    return {
+      manifestData: state.manifestReducer.manifestData
+    };
+  }
+)(SequenceMetadataPanel);
