78c931c7ac269bd2ac78235de4a6c51ddf4c5773
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-07-12 12:58:28 +0200

    Added basic styling and IIIF logo for Home component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,14 +4,18 @@ var {Link} = require('react-router');
 var Home = (props) => {
   return(
     <div className="home-container">
-      <div className="row">
-        <div className="col-md-12">
-          <Link to="/open" className="btn btn-default"><i className="fa fa-folder-open"></i> Open Manifest</Link>
+      <div className="well">
+        <img src="./img/IIIF-logo-colored-text.png" alt="IIIF Logo"/>
+        <h2>IIIF Manifest Editor</h2>
+        <div className="row">
+          <div className="col-md-12">
+            <Link to="/open" className="btn btn-default"><i className="fa fa-folder-open"></i> Open Manifest</Link>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-md-12">
-          <Link to="/new" className="btn btn-default"><i className="fa fa-file"></i> New Manifest</Link>
+        <div className="row">
+          <div className="col-md-12">
+            <Link to="/new" className="btn btn-default"><i className="fa fa-file"></i> New Manifest</Link>
+          </div>
         </div>
       </div>
     </div>
