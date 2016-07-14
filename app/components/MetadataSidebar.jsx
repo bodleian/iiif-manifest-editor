40b4fa2ee5c2b1d3a6059839f980ed4fae264fec
Chris Jesudurai <jchristo4@gmail.com>
2016-07-14 08:28:59 -0700

    Initial version of sidebar toggle

--- app/components/MetadataSidebar.jsx
+++ app/components/MetadataSidebar.jsx
@@ -1,11 +1,26 @@
 var React = require('react');
+var classNames = require('classnames');
 var MetadataSidebarButtons = require('MetadataSidebarButtons');
 var MetadataAccordion = require('MetadataAccordion');
 
 var MetadataSidebar = React.createClass({
+  getInitialState: function() {
+    return { visible: true };
+  },
+  toggle: function() {
+    this.setState({ visible: !this.state.visible });
+  },
   render: function() {
+    var sidebarClasses = classNames(
+        'col-md-4',
+        'metadata-sidebar-container',
+        {
+          visible: this.state.visible
+        }
+      );
+
     return (
-      <div className="col-md-4 metadata-sidebar-container">
+      <div className={sidebarClasses}>
         <div className="container-fluid">
           <MetadataSidebarButtons/>
         </div>
