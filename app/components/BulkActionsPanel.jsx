45eda50e19a21ef1c71958bfbe74c4242f2e2c5c
Douglas Kim <dougkim@15solutions.com>
2016-10-07 14:33:47 -0700

    Add option to select foliation side for relabeling canvases.

--- app/components/BulkActionsPanel.jsx
+++ app/components/BulkActionsPanel.jsx
@@ -3,31 +3,50 @@ var {connect} = require('react-redux');
 var actions = require('actions');
 
 var BulkActionsPanel = React.createClass({
-  renameCanvasLabelsByPagination: function() {
+  renameCanvasLabelsByPagination: function(e) {
+    e.preventDefault();
     this.props.dispatch(actions.renameCanvasLabelsByPagination(0));
   },
-  renameCanvasLabelsByFoliation: function() {
-    this.props.dispatch(actions.renameCanvasLabelsByFoliation(0, 'recto'));
+  renameCanvasLabelsByFoliation: function(e) {
+    e.preventDefault();
+    this.props.dispatch(actions.renameCanvasLabelsByFoliation(0, this.refs.foliationSide.value));
   },
   render: function() {
     return (
       <div className="metadata-sidebar-panel">
-        <div className="row">
-          <div className="col-md-12">
-            <button onClick={this.renameCanvasLabelsByPagination} className="btn btn-default"><i className="fa fa-sort-numeric-asc"></i> Rename Canvases by Pagination</button>
+        <div className="metadata-sidebar-panel-subtitle">Automatically Rename Canvases by Pagination</div>
+        <form className="form-horizontal" role="form">
+          <div className="form-group">
+            <div className="col-md-8">
+              <button onClick={this.renameCanvasLabelsByPagination} className="btn btn-default form-control"><i className="fa fa-sort-numeric-asc"></i> Rename Canvases by Pagination</button>
+            </div>
           </div>
-        </div>
+        </form>
         <div className="row">
           <div className="col-md-12">
             <div className="text-muted"><i className="fa fa-arrow-right"></i> e.g. 1, 2, 3, 4, 5...</div>
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-md-12">
-            <button onClick={this.renameCanvasLabelsByFoliation} className="btn btn-default"><i className="fa fa-sort-numeric-asc"></i> Rename Canvases by Foliation</button>
+        <hr />
+
+        <div className="metadata-sidebar-panel-subtitle">Automatically Rename Canvases by Foliation</div>
+        <form className="form-horizontal" role="form">
+          <div className="form-group">
+            <label className="col-md-3 control-label">Start With:</label>
+            <div className="col-md-3">
+              <select ref="foliationSide" className="form-control">
+                <option value="recto">Recto</option>
+                <option value="verso">Verso</option>
+              </select>
+            </div>
           </div>
-        </div>
+          <div className="form-group">
+            <div className="col-md-8">
+              <button onClick={this.renameCanvasLabelsByFoliation} className="btn btn-default form-control"><i className="fa fa-sort-numeric-asc"></i> Rename Canvases by Foliation</button>
+            </div>
+          </div>
+        </form>
         <div className="row">
           <div className="col-md-12">
             <div className="text-muted"><i className="fa fa-arrow-right"></i> e.g. 1r, 1v, 2r, 2v, 3r, 3v...</div>
