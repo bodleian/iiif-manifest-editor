0ff6e016558a41d71dae6d62f0292e9fd8b33f59
Douglas Kim <dougkim@15solutions.com>
2016-10-11 15:21:12 -0700

    Display non-empty metadata fields in manifest metadata dialog. Redesign metadata fields in manifest metadata dialog with additional spacing and font weights.

--- app/components/SourceManifestMetadataDialog.jsx
+++ app/components/SourceManifestMetadataDialog.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var SourceManifestMetadataDialog = React.createClass({
   render: function() {
     return (
-      <div className="modal fade">
+      <div className="modal fade manifest-metadata-dialog">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -12,42 +12,74 @@ var SourceManifestMetadataDialog = React.createClass({
             </div>
             <div className="modal-body">
               <div className="source-manifest-metadata">
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">Manifest URI</div>
-                  <div className="col-md-9 metadata-field-value">
-                    <a href={this.props.manifestData.id} target="_blank">{this.props.manifestData.id}</a>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">Label</div>
-                  <div className="col-md-9 metadata-field-value">
-                    {this.props.manifestData.getLabel()}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">Description</div>
-                  <div className="col-md-9 metadata-field-value">
-                    {this.props.manifestData.getDescription()}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">Attribution</div>
-                  <div className="col-md-9 metadata-field-value">
-                    {this.props.manifestData.getAttribution()}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">License</div>
-                  <div className="col-md-9 metadata-field-value">
-                    {this.props.manifestData.getLicense()}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 metadata-field-label">Logo</div>
-                  <div className="col-md-9 metadata-field-value">
-                    {this.props.manifestData.getLogo()}
-                  </div>
-                </div>
+                {(() => {
+                  var manifestId = this.props.manifestData.id;
+                  if(manifestId !== undefined && manifestId !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">Manifest URI</div>
+                        <div className="col-md-9 metadata-field-value">
+                          <a href={manifestId} target="_blank">{manifestId}</a>
+                        </div>
+                      </div>
+                    );
+                  }
+                })()}
+                {(() => {
+                  var label = this.props.manifestData.getLabel();
+                  if(label !== undefined && label !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">Label</div>
+                        <div className="col-md-9 metadata-field-value">{label}</div>
+                      </div>
+                    );
+                  }
+                })()}
+                {(() => {
+                  var description = this.props.manifestData.getDescription();
+                  if(description !== undefined && description !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">Description</div>
+                        <div className="col-md-9 metadata-field-value">{description}</div>
+                      </div>
+                    );
+                  }
+                })()}
+                {(() => {
+                  var attribution = this.props.manifestData.getAttribution();
+                  if(attribution !== undefined && attribution !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">Attribution</div>
+                        <div className="col-md-9 metadata-field-value">{attribution}</div>
+                      </div>
+                    );
+                  }
+                })()}
+                {(() => {
+                  var license = this.props.manifestData.getLicense();
+                  if(license !== undefined && license !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">License</div>
+                        <div className="col-md-9 metadata-field-value">{license}</div>
+                      </div>
+                    );
+                  }
+                })()}
+                {(() => {
+                  var logo = this.props.manifestData.getLogo();
+                  if(logo !== undefined && logo !== '') {
+                    return (
+                      <div className="row metadata-field-row">
+                        <div className="col-md-3 metadata-field-label">Logo</div>
+                        <div className="col-md-9 metadata-field-value">{logo}</div>
+                      </div>
+                    );
+                  }
+                })()}
               </div>
             </div>
             <div className="modal-footer">
