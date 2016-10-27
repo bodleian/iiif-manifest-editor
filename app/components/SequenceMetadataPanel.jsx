2e1438cd6339dd366ad2fac5cca462057d5c4511
Rafael Schwemmer <rafael.schwemmer@gmail.com>
2016-10-27 15:49:42 +0200

    Added drop-down menu to Sequence Metadata panel to toggle viewing direction. Resolves issue #18

--- app/components/SequenceMetadataPanel.jsx
+++ app/components/SequenceMetadataPanel.jsx
@@ -16,6 +16,19 @@ var SequenceMetadataPanel = React.createClass({
           <div className="col-md-3 metadata-field-label">Sequence Label</div>
           <EditableTextArea classNames="col-md-9 metadata-field-value" fieldValue={sequence.getLabel()} path="sequences/0/label" onUpdateHandler={this.saveMetadataFieldToStore}/>
         </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="btn-group">
+              <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                Set Viewing Direction <span className="caret"></span>
+              </button>
+              <ul className="dropdown-menu" role="menu">
+                <li><a href="javascript:;" onClick={() => this.saveMetadataFieldToStore('left-to-right', 'viewingDirection')}>Left to Right</a></li>
+                <li><a href="javascript:;" onClick={() => this.saveMetadataFieldToStore('right-to-left', 'viewingDirection')}>Right to Left</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
