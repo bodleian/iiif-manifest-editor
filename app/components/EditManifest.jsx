8c8c808fd24fa2d6df2f6a9ed1c93f614d99f3f9
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-11-12 22:36:54 +0100

    Moved OpenSeadragon component from EditManifest component to Viewer component. Display initial image in OpenSeadragon. Needs correct updating of main image.

--- app/components/EditManifest.jsx
+++ app/components/EditManifest.jsx
@@ -5,26 +5,7 @@ var classNames = require('classnames');
 var Viewer = require('Viewer');
 var MetadataSidebar = require('MetadataSidebar');
 var ThumbnailStrip = require('ThumbnailStrip');
-import ReactDOM from 'react-dom'
-import OpenseadragonViewer from 'react-openseadragon'
-import OpenSeadragonControls from 'react-openseadragon'
-import OpenSeadragonNavigator from 'react-openseadragon'
 
-
-var openSeaDragonConfig = {
-      sequenceMode:  false,
-      showReferenceStrip: false,
-      defaultZoomLevel: 0,
-      tileSources:   [
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000001.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000002.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000003.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000004.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000005.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000006.jp2/info.json",
-          "http://libimages.princeton.edu/loris2/pudl0001%2F4609321%2Fs42%2F00000007.jp2/info.json"
-      ]
-  }
 
 var EditManifest = React.createClass({
   componentDidMount: function() {
@@ -59,7 +40,7 @@ var EditManifest = React.createClass({
         <div className="edit-manifest-container container-fluid">
           <div className="row">
             <div className={viewerThumbnailStripClasses}>
-              <OpenseadragonViewer config={openSeaDragonConfig} />
+              <Viewer/>
               <ThumbnailStrip/>
             </div>
             <MetadataSidebar ref="sidebar"/>
