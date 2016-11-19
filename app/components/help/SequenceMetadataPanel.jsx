42336fa049c3185bacb459b00846f926ba35c214
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2016-11-19 17:16:54 +0100

    Added on-screen help text for sequence metadata panel

--- app/components/help/SequenceMetadataPanel.jsx
+++ app/components/help/SequenceMetadataPanel.jsx
@@ -4,7 +4,22 @@ module.exports = function(props) {
   return (
   	<div>
   	  <h3>The Sequence Metadata Panel</h3>
-  	  <p></p>
+  	  <p>
+  	  	The Sequence Metadata panel let&apos;s you edit the label for the current sequence and set the viewing direction for the manifest. 
+  	  </p>
+
+  	  <h4>Setting Viewing Direction</h4>
+  	  <p>	
+  	  	Use the drop-down menu to toggle between <i>left-to-right</i> and <i>right-to-left</i> viewing directions. 
+  	  	The <i>viewingDirection</i> field in the Manifest Metadata panel will be updated accordingly or added to the manifest level 
+  	  	metadata if it has not been there before. You can also edit this field in the Manifest Metadata panel directly. 
+  	  </p>
+
+  	  <p>
+  	  	If <i>right-to-left</i> has been selected, the canvases in the thumbnail strip will be displayed in reverse order. 
+  	  	This setting only affects the <i>display</i> of the canvases in the thumbnail strip and does not actually reverse the order of 
+  	  	the canvases in the manifest itself.
+  	  </p>
   	</div>
   );
 };
