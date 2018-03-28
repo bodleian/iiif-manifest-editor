9d20aee1ed67aafd7609350c2facdbccd0471caa
Douglas Kim <dougkim@15solutions.com>
2018-03-28 15:16:40 -0700

    Rename addMetadataField function to addMetadataProperty.

--- app/components/metadata-sidebar/ManifestMetadataPanelCustomFields.jsx
+++ app/components/metadata-sidebar/ManifestMetadataPanelCustomFields.jsx
@@ -25,7 +25,7 @@ var ManifestMetadataPanelCustomFields = React.createClass({
       });
     }
   },
-  addMetadataField: function(fieldLabel, fieldValue, addPath) {
+  addMetadataProperty: function(fieldLabel, fieldValue, addPath) {
     this.props.dispatch(actions.addMetadataFieldToListAtPath(fieldValue, addPath));
   },
   updateMetadataPropertyValue: function(fieldType, updatePath, fieldName, fieldValue) {
@@ -94,11 +94,11 @@ var ManifestMetadataPanelCustomFields = React.createClass({
             }
           })
         }
-        <button type="button" className="btn btn-default add-metadata-field-button" title="Add metadata field" onClick={() => _this.addMetadataField('Label', { 'label': 'Label', 'value': 'Value' }, 'metadata')}>
+        <button type="button" className="btn btn-default add-metadata-field-button" title="Add metadata field" onClick={() => _this.addMetadataProperty('Label', { 'label': 'Label', 'value': 'Value' }, 'metadata')}>
           <span className="fa fa-plus"></span> Add metadata field
         </button>
 
-        <button type="button" className="btn btn-default" title="Add language metadata field" onClick={() => _this.addMetadataField('Label', { 'label': 'Label', 'value': { '@value': '', '@language': '' } }, 'metadata')}>
+        <button type="button" className="btn btn-default" title="Add language metadata field" onClick={() => _this.addMetadataProperty('Label', { 'label': 'Label', 'value': { '@value': '', '@language': '' } }, 'metadata')}>
           <span className="fa fa-plus"></span> Add language metadata field
         </button>
       </div>
