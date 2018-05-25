4211d98b2dcc7cc390ac697ad28060c99c4bf842
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2018-05-25 11:27:32 +0200

    Update help text for canvas metadata to cover related field and custom fields

--- app/components/help/CanvasMetadataPanel.jsx
+++ app/components/help/CanvasMetadataPanel.jsx
@@ -5,8 +5,10 @@ module.exports = function(props) {
   	<div>
   	  <h3>The Canvas Metadata Panel</h3>
   	  <p>
-  	  	The Canvas Metadata panel shows the metadata for the selected canvas. You can edit the canvas label, the width and the height 
-  	  	of the canvas and the image URI for the image that is annotated to the selected canvas.
+  	  	The Canvas Metadata panel shows the metadata for the selected canvas. You can edit 
+        the canvas label, the width and the height of the canvas and the image URI for the 
+        image that is annotated to the selected canvas. You can also specify a "related"
+        property with a link to an external resource.
   	  </p>
   	  <h4>Replace an Image on a Canvas or Add an Image to an Empty Canvas</h4>
   	  <p>
@@ -29,6 +31,32 @@ module.exports = function(props) {
   	      <br /><img className="iiif-icon" src="./img/IIIF-logo-colored-text.png" height="15" alt="IIIF Logo"/> Documentation: <a href="http://iiif.io/api/presentation/2.1/#image-resources" target="_blank">http://iiif.io/api/presentation/2.1/#image-resources</a>
   	    </li>
   	  </ul>
+      <h4>The "related" Field</h4>
+      <p>
+        A link to an external resource intended to be displayed directly to the user that
+        is related to the canvas. Examples might include a website describing the canvas, 
+        an HTML description, etc. A label and the format of the related resource should be 
+        given to assist clients in rendering the resource to the user.
+      </p>
+
+      <h4>Custom fields</h4>
+      <p>
+        Custom fields are value/label pairs that will be stored in the "metadata" section of the canvas. 
+        "Custom" here means that the <i>Label</i> for each field is not predefined and can be set to 
+        any string.
+        Click the "Add metadata field button" to add a custom field. This will add a new box with "Label" 
+        for the label of the new property and "Value" for the value:
+      </p>
+      <p>To edit the <i>label</i>, click on it, replace the default text of "Label" with the name for 
+      the metadata field (e.g. "Date") and hit enter. Do the same for the <i>value</i> field.</p>
+
+      <p>
+        You can also add a language-specific custom field by clicking on the "Add language metadata field"
+        button. In addition to the "Label" and "Value" fields there is a "Language" field. Specify the 
+        language for the metadata field as a two-letter&nbsp;
+        <a target="_blank" href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes">ISO-639-1 code</a>&nbsp;
+        such as "en" for English.
+      </p>
   	</div>
   );
 };
