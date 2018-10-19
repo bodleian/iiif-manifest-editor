72c2bfff7a8b7abc76d47b2a4722ee12ccb3d289
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2018-10-19 22:11:57 +0200

    Initialize the label to "Empty canvas" for empty canvases in canvas selector dialog for manifest thumbnail

--- app/components/metadata-sidebar/MetadataSidebarCanvas.jsx
+++ app/components/metadata-sidebar/MetadataSidebarCanvas.jsx
@@ -15,7 +15,7 @@ var MetadataSidebarCanvas = React.createClass({
       <div style={{background: '#fff url(./img/loading-small.gif) no-repeat center center'}} className="metadata-sidebar-canvas">
         <img src={this.getMainImage(canvas)} alt={canvas !== null ? Utils.getLocalizedPropertyValue(canvas.getLabel()) : ''} height="150" />
         <div className="canvas-label">
-          {canvas !== null ? Utils.getLocalizedPropertyValue(canvas.getLabel()) : ''}
+          {canvas !== null ? Utils.getLocalizedPropertyValue(canvas.getLabel()) : 'Empty canvas'}
         </div>
       </div>
     );
